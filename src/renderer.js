7f5968d78ec4a9063feefd4b43fcb10487fda18f
majestic380 <majestic_aic@hotmail.fr>
2026-01-08 03:10:41 +0100

    Delete src directory

--- src/renderer.js
+++
@@ -1,4 +0,0 @@
-﻿document.getElementById("btnPing").addEventListener("click", async () => {
-  const res = await window.btp.ping();
-  document.getElementById("out").textContent = JSON.stringify(res, null, 2);
-});
